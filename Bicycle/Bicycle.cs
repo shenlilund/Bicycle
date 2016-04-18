8343c442976f36b43fe07aec034e3fc612b762d8
shenlilund <shenlilund@gmail.com>
2016-04-18 11:31:48 -0500

    Added class files
    Added Bicycle, Cruiser, MountainBike, BMX, and Wheel classes

---
+++ Bicycle/Bicycle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bicycle
+{
+    class Bicycle
+    {
+        Wheel wheel1;
+        Wheel wheel2;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
---
+++ Bicycle/Wheel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bicycle
+{
+    class Wheel
+    {
+        int diameter;
+        String tireType;
+    }
+}
